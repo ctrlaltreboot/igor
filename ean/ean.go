a76cbd74b0d34fb1847d80ec968cf9290dedff70
r8j <robot@rebooter.xyz>
2017-08-18 22:09:53 +1000

    Fix structs. You don't need to add all field. Use only those you need.

--- ean/ean.go
+++ ean/ean.go
@@ -1,106 +1,50 @@
 package ean
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ctrlaltreboot/igor/helper"
 	"net/http"
 )
 
 type EanResponse struct {
-	CustomerSessionID      string `json:"customerSessionId"`
-	NumberOfRoomsRequested int32  `json:"numberOfRoomsRequested"`
-	MoreResultsAvailable   bool   `json:"moreResultsAvailable"`
-	CacheKey               string `json:"cacheKey"`
-	CacheLocation          string `json:"cacheLocation"`
-	CachedSupplierResponse map[string]interface{}
-	HotelList              HotelList `json:"HotelList"`
+	HotelList HotelList `json:"HotelList"`
 }
 
 type HotelList struct {
-	Size                string         `json:"@size"`
-	ActivePropertyCount string         `json:"activePropertyCount"`
-	HotelSummary        []HotelSummary `json:"HotelSummary"`
+	HotelSummary []HotelSummary `json:"HotelSummary"`
 }
 
 type HotelSummary struct {
-	Order                  string              `json:"@order"`
-	UbsScore               string              `json:"@ubsScore"`
-	HotelId                string              `json:"hotelId"`
-	Name                   string              `json:"name"`
-	Address1               string              `json:"address1"`
-	City                   string              `json:"city"`
-	StateProvinceCode      string              `json:"stateProvinceCode"`
-	PostalCode             string              `json:"postalCode"`
-	CountryCode            string              `json:"countryCode"`
-	AirportCode            string              `json:"airportCode"`
-	SupplierType           string              `json:"supplierType"`
-	PropertyCategory       string              `json:"propertyCategory"`
-	HotelRating            string              `json:"hotelRating"`
-	HotelRatingDisplay     string              `json:"hotelRatingDisplay"`
-	ConfidenceRating       string              `json:"confidenceRating"`
-	AmenityMask            string              `json:"amenityMask"`
-	TripAdvisorRating      string              `json:"tripAdvisorRating"`
-	TripAdvisorReviewCount string              `json:"tripAdvisorReviewCount"`
-	TripAdvisorRatingUrl   string              `json:"tripAdvisorRatingUrl"`
-	LocationDescription    string              `json:"locationDescription"`
-	ShortDescription       string              `json:"shortDescription"`
-	HighRate               string              `json:"highRate"`
-	LowRate                string              `json:"lowRate"`
-	RateCurrencyCode       string              `json:"rateCurrencyCode"`
-	Latitude               string              `json:"latitude"`
-	Longitude              string              `json:"longitude"`
-	ProximityDistance      string              `json:"proximityDistance"`
-	ProximityUnit          string              `json:"proximityUnit"`
-	HotelInDestination     string              `json:"hotelInDestination"`
-	ThumbNailUrl           string              `json:"thumbNailUrl"`
-	DeepLink               string              `json:"deepLink"`
-	RoomRateDetailsList    RoomRateDetailsList `json:"RoomRateDetailsList"`
-}
-
-type RoomRateDetailsList struct {
-	RoomRateDetails RoomRateDetails `json:"RoomRateDetails"`
+	HotelId             string          `json:"hotelId"`
+	RoomRateDetailsList RoomRateDetails `json:"RoomRateDetailsList"`
 }
 
 type RoomRateDetails struct {
-	RoomTypeCode        string    `json:"roomTypeCode"`
-	RateCode            string    `json:"rateCode"`
-	MaxRoomOccupancy    string    `json:"maxRoomOccupancy"`
-	QuotedRoomOccupancy string    `json:"quotedRoomOccupancy"`
-	MinGuestAge         string    `json:"minGuestAge"`
-	RoomDescription     string    `json:"roomDescription"`
-	PropertyAvailable   bool      `json:"propertyAvailable"`
-	PropertyRestricted  bool      `json:"propertyRestricted"`
-	ExpediaPropertyId   string    `json:"expediaPropertyId"`
-	RateInfos           RateInfos `json:"RateInfos`
+	RoomTypeCode    string    `json:"roomTypeCode"`
+	RoomDescription string    `json:"roomDescription"`
+	RateInfos       RateInfos `json:"RateInfos`
 }
 
 type RateInfos struct {
-	Size             string   `json:"@size"`
-	RateInfo         RateInfo `json:"RateInfo"`
-	NonRefundable    string   `json:"nonRefundable"`
-	RateType         string   `json:"rateType"`
-	PromoId          string   `json:"promoId"`
-	PromoDescription string   `json:"promoDescription"`
-	PromoType        string   `json:"promoType"`
-	CurrentAllotment string   `json:"currentAllotment"`
+	RateInfo      RateInfo `json:"RateInfo"`
+	NonRefundable string   `json:"nonRefundable"`
 }
 
 type RateInfo struct {
-	PriceBreakdown     string             `json:"@priceBreakdown"`
-	Promo              string             `json:"@promo"`
-	RateChange         string             `json:"@rateChange"`
-	RoomGroup          Room               `json:"RoomGroup"`
 	ChargeableRateInfo ChargeableRateInfo `json:"ChargeableRateInfo"`
 }
 
-type Room struct {
-	numberOfAdults
-	numberOfChildren
-	rateKey
-	ChargeableNightlyRates []ChargeableNightlyRate `json:"ChargeableNightlyRates"`
+type ChargeableRateInfo struct {
+	Total string `json:"@total"`
 }
 
-type ChargeableNightlyRate struct {
+type CheapestOffer struct {
+	PropertyName  string `json:"property_name"`
+	RoomTypeName  string `json:"room_type_name"`
+	OfferName     string `json:"offer_name"`
+	Amount        string `json:"amount"`
+	NonRefundable string `json:"is_non_refundable"`
 }
 
 func List(w http.ResponseWriter, r *http.Request) {
@@ -114,7 +58,15 @@ func List(w http.ResponseWriter, r *http.Request) {
 }
 
 func Cheapest(w http.ResponseWriter, r *http.Request) {
+	b, err := helper.Fetch("http://127.0.0.1:5092/ean")
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	}
 
-	w.Header().Set("Content-Type", "application/json")
+	var e EanResponse
+	err = json.Unmarshal(b, &e)
+	for _, deets := range e.HotelList.HotelSummary {
+		fmt.Fprintf(w, "%s\n", deets)
+	}
 	fmt.Fprintf(w, "%s", b)
 }
