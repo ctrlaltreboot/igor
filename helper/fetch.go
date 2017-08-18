7ca71b7a7dab86826fbeaa50153a4640b56178b6
r8j <robot@rebooter.xyz>
2017-08-18 16:56:08 +1000

    Add helper package.

---
+++ helper/fetch.go
@@ -0,0 +1,20 @@
+package helper
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func Fetch(url string) ([]byte, error) {
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, err
+}
