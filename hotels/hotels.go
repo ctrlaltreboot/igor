cfd1266f482caca6be1a268226789b9efa9708f6
r8j <robot@rebooter.xyz>
2017-08-19 08:19:19 +1000

    Reduce the amount of fluff.

--- hotels/hotels.go
+++ hotels/hotels.go
@@ -9,45 +9,19 @@ import (
 )
 
 type HotelsResponse struct {
-	Meta       Meta       `json:"meta"`
 	Properties []Property `json:"properties"`
 }
 
-type Meta struct {
-	Query `json:"query"`
-}
-
-type Query struct {
-	Brand       string   `json:"brand"`
-	OfferType   string   `json:"offer_type"`
-	CheckIn     string   `json:"check_in"`
-	CheckOut    string   `json:"check_out"`
-	Adults      string   `json:"adults"`
-	Children    string   `json:"children"`
-	Infants     string   `json:"infants"`
-	PropertyIds []string `json:"property_ids"`
-}
-
 type Property struct {
-	Id        string     `json:"id"`
 	RoomTypes []RoomType `json:"room_types"`
 }
 
 type RoomType struct {
-	Id     string  `json:"id"`
 	Offers []Offer `json:"offers"`
 }
 
 type Offer struct {
-	Id                    string             `json:"id"`
-	Name                  string             `json:"name"`
-	Description           string             `json:"description"`
-	Charges               Charges            `json:"charges"`
-	CancellationPolicy    CancellationPolicy `json:"cancellation_policy"`
-	Promotion             Promotion          `json:"promotion"`
-	BaseIncludedOccupants int32              `json:"base_included_occupants"`
-	ExtraOccupantCount    int32              `json:"extra_occupant_count"`
-	Type                  string             `json:"type"`
+	Charges Charges `json:"charges"`
 }
 
 type Charges struct {
@@ -63,19 +37,6 @@ type Charges struct {
 type Charge struct {
 	Amount   string `json:"amount"`
 	Currency string `json:"currency"`
-}
-
-type CancellationPolicy struct {
-	IsRefundable bool   `json:"is_refundable"`
-	Description  string `json:"description"`
-}
-
-type Promotion struct {
-	PromotionCode string `json:"promotion_code"`
-	Name          string `json:"name"`
-	Description   string `json:"description"`
-	CampaignCode  string `json:"campaign_code"`
-	Priority      int64  `json:"priority"`
 }
 
 type CheapestHandler struct {
